be36a31076236d6f17628b8d411d38b2ab510de0
SylvainLEFORT <Lefort10700@hotmail.com>
2022-04-01 13:54:53 +0200

    first commit

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,10 @@
 // == Import
-import reactLogo from './react-logo.svg';
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    Bientôt : du contenu !
   </div>
 );
 
