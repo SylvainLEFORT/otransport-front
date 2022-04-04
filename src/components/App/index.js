e2aa256868549ed39be7dcea15c53ad845bdcd1a
FrancoisBoudin <Boudinfrancois@hotmail.com>
2022-04-04 09:38:21 +0200

    création routes / et /error et leurs dossiers

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,21 @@
 //   Import
+import { Route, Routes } from 'react-router-dom';
 import './styles.scss';
 import 'semantic-ui-css/semantic.min.css';
 //  Composant
 import Header from '../Header';
+import Login from '../Login';
+import Error from '../Error';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+
   </div>
 );
 
---
+++ src/components/Error/index.js
@@ -0,0 +1,9 @@
+import './error.scss';
+
+const Error = () => (
+  <div>
+    404 NO FOUND
+  </div>
+);
+
+export default Error;
