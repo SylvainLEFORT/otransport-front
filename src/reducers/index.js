094cd4c9fc2fb6502a355c452621194b366f64c2
FrancoisBoudin <Boudinfrancois@hotmail.com>
2022-04-04 08:52:46 +0200

    initialisation du reducer, store, et création du dossier actions ainsi que le router

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+
+// import des differents reducers
+
+const rootReducer = combineReducers({
+  // nom du tiroir: reducer qui gère cette partie du state
+
+});
+
+export default rootReducer;
