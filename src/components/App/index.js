561cb714133d8ebfcafd41a90653480844e1b3c1
FrancoisBoudin <Boudinfrancois@hotmail.com>
2022-04-04 10:24:33 +0200

    incrustation statique page login

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -10,7 +10,7 @@ import Error from '../Error';
 // == Composant
 const App = () => (
   <div className="app">
-    <Header />
+    {/* <Header /> */}
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="*" element={<Error />} />
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,8 +1,23 @@
 import './login.scss';
+import { Image, Form, Button } from 'semantic-ui-react';
+import logo from 'src/assets/docs/O-transport.svg';
 
 const Login = () => (
-  <div>
-    Bientot ici la page de Login
+  <div className="login">
+    <Image className="logo" src={logo} size="medium" />
+    <div className="form">
+      <Form>
+        <Form.Field>
+          <label>Nom d'utilisateur</label>
+          <input placeholder="veillez ecrire votre nom" />
+        </Form.Field>
+        <Form.Field>
+          <label>Mot de passe</label>
+          <input type="password" placeholder="veillez ecrire votre mot de passe" />
+        </Form.Field>
+        <Button type="submit">Se connecter</Button>
+      </Form>
+    </div>
   </div>
 );
 
