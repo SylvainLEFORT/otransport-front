025a1daf55df2dbcd39d8ad34bc473e4c1e8e948
FrancoisBoudin <Boudinfrancois@hotmail.com>
2022-04-04 09:00:15 +0200

    création du inital state et implémentation du combinereducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
+import reducer from './reducer';
 
 // import des differents reducers
 
 const rootReducer = combineReducers({
   // nom du tiroir: reducer qui gère cette partie du state
-
+  reducer: reducer,
 });
 
 export default rootReducer;
