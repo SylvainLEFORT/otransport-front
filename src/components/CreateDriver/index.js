fa6b19076eb3d96c4cad0316709706897d8e29df
FrancoisBoudin <Boudinfrancois@hotmail.com>
2022-04-05 13:38:29 +0200

    tamere

--- src/components/CreateDriver/index.js
+++ src/components/CreateDriver/index.js
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Form } from 'semantic-ui-react';
+import { Button, Form } from 'semantic-ui-react';
 import NavBar from '../NavBar';
 import './createdriver.scss';
 
@@ -6,31 +6,31 @@ const CreateDriver = () => (
   <div>
     <NavBar />
     <div className="form">
-      <h1>Création d'un chauffeur</h1>
+
       <Form>
         <Form.Field>
           <label>Prénom</label>
-          <input placeholder="First Name" />
+          <input placeholder="Veillez inséré le prénom" />
         </Form.Field>
         <Form.Field>
           <label>Nom</label>
-          <input placeholder="Last Name" />
+          <input placeholder="Veillez inséré le nom" />
         </Form.Field>
         <Form.Field>
           <label>E-mail</label>
-          <input placeholder="Last Name" />
+          <input placeholder="Veillez inséré l'e-mail" />
         </Form.Field>
         <Form.Field>
           <label>Mot de passe</label>
-          <input placeholder="Last Name" />
+          <input placeholder="Veillez inséré le mot de passe" />
         </Form.Field>
         <Form.Field>
           <label>Numéro de téléphone</label>
-          <input placeholder="Last Name" />
+          <input placeholder="Veillez inséré le numéro de téléphone" />
         </Form.Field>
         <Form.Field>
           <label>Image de profil</label>
-          <input placeholder="Last Name" />
+          <input type="file" placeholder="Last Name" />
         </Form.Field>
         <Button type="submit">Créer un chauffeur</Button>
       </Form>
