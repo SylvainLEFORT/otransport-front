ea301ea55a367c9919b4841682224b3b79438856
SylvainLEFORT <Lefort10700@hotmail.com>
2022-04-06 13:15:43 +0200

    create driver

--- src/components/CreateDriver/index.js
+++ src/components/CreateDriver/index.js
@@ -11,6 +11,8 @@ export default class CreateDriver extends React.Component {
     firstname: '',
     lastname: '',
     email: '',
+    password: '',
+    phone_number: '',
   };
 
   handleChange = (event) => {
@@ -18,24 +20,29 @@ export default class CreateDriver extends React.Component {
       firstname: event.target.value,
       lastname: event.target.value,
       email: event.target.value,
+      password: event.target.value,
+      phone_number: event.target.value,
     });
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
 
-    const driver = {
+    const driver = JSON.stringify({
       firstname: this.state.firstname,
       lastname: this.state.lastname,
       email: this.state.email,
+      password: this.state.password,
+      phone_number: this.state.phone_number,
 
-    };
+    });
 
-    axios.post('http://localhost:8000/api/drivers', { driver })
+    axios.post('http://localhost:8000/api/drivers', { driver }, { headers: { 'Content-Type': 'application/json' } })
       .then((res) => {
         console.log(res);
         console.log(res.data);
       });
+    console.log(driver);
   };
 
   render() {
@@ -47,28 +54,29 @@ export default class CreateDriver extends React.Component {
           <Form onSubmit={this.handleSubmit}>
             <Form.Field className="input-1">
               <label>Prénom</label>
-              <input placeholder="Veillez insérer le prénom" firstname="firstname" onChange={this.handleChange} />
+              <input placeholder="Veillez insérer le prénom" name="firstname" onChange={this.handleChange} />
             </Form.Field>
             <Form.Field className="input-1">
               <label>Nom</label>
-              <input placeholder="Veillez insérer le nom" lastname="lastname" onChange={this.handleChange} />
+              <input placeholder="Veillez insérer le nom" name="lastname" onChange={this.handleChange} />
             </Form.Field>
             <Form.Field className="input-1">
               <label>E-mail</label>
-              <input placeholder="Veillez insérer l'e-mail" email="email" onChange={this.handleChange} />
+              <input placeholder="Veillez insérer l'e-mail" name="email" onChange={this.handleChange} />
             </Form.Field>
-            {/* <Form.Field className="input-1">
+            <Form.Field className="input-1">
               <label>Mot de passe</label>
-              <input placeholder="Veillez insérer le mot de passe" onChange={this.handleChange} />
+              <input placeholder="Veillez insérer le mot de passe" name="password" onChange={this.handleChange} />
             </Form.Field>
             <Form.Field className="input-1">
               <label>Numéro de téléphone</label>
               <input
-              placeholder="Veillez insérer le numéro de téléphone"
-              onChange={this.handleChange}
+                placeholder="Veillez insérer le numéro de téléphone"
+                name="phone_number"
+                onChange={this.handleChange}
               />
             </Form.Field>
-            <Form.Field>
+            {/* <Form.Field>
               <label>Image de profil</label>
               <input type="file" placeholder="Last Name" onChange={this.handleChange} />
             </Form.Field> */}
