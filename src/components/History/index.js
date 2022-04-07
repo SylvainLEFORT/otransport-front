82573946d83d7fecc98485564717376a688a4243
SylvainLEFORT <Lefort10700@hotmail.com>
2022-04-07 09:42:17 +0200

    peaufinage affichage

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -4,55 +4,57 @@ import NavBar from '../NavBar';
 const History = () => (
   <div>
     <NavBar />
-    <h1 className="title">Historique des livraisons</h1>
-    <div className="trait" />
-    <div className="deliveries-management">
-      <ul>
-        <li className="delivery">
-          <div className="detail-1">
-            <span>Nom du client</span>
-            <span>Adresse du client</span>
-            <span>Descriptif livraison : cargaison + volume</span>
-          </div>
-          <div className="detail-2">
-            <span>Attribuée à : chauffeur 1</span>
-            <span>Date de livraison</span>
-          </div>
-        </li>
-        <li className="delivery">
-          <div className="detail-1">
-            <span>Nom du client</span>
-            <span>Adresse du client</span>
-            <span>Descriptif livraison : cargaison + volume</span>
-          </div>
-          <div className="detail-2">
-            <span>Attribuée à : chauffeur 83</span>
-            <span>Date de livraison</span>
-          </div>
-        </li>
-        <li className="delivery">
-          <div className="detail-1">
-            <span>Nom du client</span>
-            <span>Adresse du client</span>
-            <span>Descriptif livraison : cargaison + volume</span>
-          </div>
-          <div className="detail-2">
-            <span>Attribuée à : chauffeur 4</span>
-            <span>Date de livraison</span>
-          </div>
-        </li>
-        <li className="delivery">
-          <div className="detail-1">
-            <span>Nom du client</span>
-            <span>Adresse du client</span>
-            <span>Descriptif livraison : cargaison + volume</span>
-          </div>
-          <div className="detail-2">
-            <span>Attribuée à : chauffeur 5</span>
-            <span>Date de livraison</span>
-          </div>
-        </li>
-      </ul>
+    <div className="historique">
+      <h1 className="title">Historique des livraisons</h1>
+      <div className="trait" />
+      <div className="deliveries-management">
+        <ul>
+          <li className="delivery">
+            <div className="detail-1">
+              <span>Nom du client</span>
+              <span>Adresse du client</span>
+              <span>Descriptif livraison : cargaison + volume</span>
+            </div>
+            <div className="detail-2">
+              <span>Attribuée à : chauffeur 1</span>
+              <span>Date de livraison</span>
+            </div>
+          </li>
+          <li className="delivery">
+            <div className="detail-1">
+              <span>Nom du client</span>
+              <span>Adresse du client</span>
+              <span>Descriptif livraison : cargaison + volume</span>
+            </div>
+            <div className="detail-2">
+              <span>Attribuée à : chauffeur 83</span>
+              <span>Date de livraison</span>
+            </div>
+          </li>
+          <li className="delivery">
+            <div className="detail-1">
+              <span>Nom du client</span>
+              <span>Adresse du client</span>
+              <span>Descriptif livraison : cargaison + volume</span>
+            </div>
+            <div className="detail-2">
+              <span>Attribuée à : chauffeur 4</span>
+              <span>Date de livraison</span>
+            </div>
+          </li>
+          <li className="delivery">
+            <div className="detail-1">
+              <span>Nom du client</span>
+              <span>Adresse du client</span>
+              <span>Descriptif livraison : cargaison + volume</span>
+            </div>
+            <div className="detail-2">
+              <span>Attribuée à : chauffeur 5</span>
+              <span>Date de livraison</span>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 );
