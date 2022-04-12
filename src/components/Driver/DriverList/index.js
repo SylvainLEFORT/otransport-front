01605a91d132ff5e70491822ba1786e6cbdf1bee
SylvainLEFORT <Lefort10700@hotmail.com>
2022-04-12 15:07:09 +0200

    rewrite and dynamisation done

--- src/components/Driver/DriverList/index.js
+++ src/components/Driver/DriverList/index.js
@@ -1,37 +1,59 @@
 import './driverlist.scss';
 
 import { Button } from 'semantic-ui-react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Mediaquery from 'react-responsive';
 
-const DriverList = () => (
-  <div className="gobal">
-    <Mediaquery minWidth={601}>
-      <div className="driverList">
-        <div className="infosClient">
-          <p className="client">Nom du client</p>
-          <p className="adresse">Adresse</p>
-        </div>
-        <div className="utilitaire">
-          <p className="statut">Statut de la livraison</p>
-          <a href="http://localhost:8080/driver/delivery/1">
-            <Button className="button" type="submit">Détails</Button>
-          </a>
-        </div>
-      </div>
-    </Mediaquery>
-    <Mediaquery maxWidth={600}>
-      <div className="driverList-phone">
-        <div className="infosClient-phone">
-          <p className="client">Nom du client</p>
-        </div>
-        <div className="utilitaire-phone">
-          <a href="http://localhost:8080/driver/delivery/1">
-            <Button className="button" type="submit">Détails</Button>
-          </a>
-        </div>
-      </div>
-    </Mediaquery>
-  </div>
-);
+const DriverList = () => {
+  const [deliveries, setDeliveries] = useState();
+
+  useEffect(() => {
+    axios.get('http://0.0.0.0:8000/api/admin/deliveries/shipping')
+      .then((res) => {
+        const resultDeliveries = res.data;
+        console.log(resultDeliveries);
+        setDeliveries(resultDeliveries);
+      });
+  }, []);
+
+  return (
+    <div className="gobal">
+      {deliveries && deliveries.map((item) => (
+        <Mediaquery maxWidth={600}>
+          <div className="driverList">
+            <div className="infosClient">
+              <p className="client">{item.customer.name}</p>
+              <p className="adresse">{item.customer.address}</p>
+            </div>
+            <div className="utilitaire">
+              <p className="statut">{(() => {
+                switch (item.status) {
+                  case 0: return 'En attende de livraison';
+                  case 1: return 'En cours de livraison';
+                  default: return '';
+                }
+              })()}
+              </p>
+              <a href={`http://localhost:8080/driver/delivery/${item.id}`}>
+                <Button className="button" type="submit">Détails</Button>
+              </a>
+            </div>
+          </div>
+          <div className="driverList-phone">
+            <div className="infosClient-phone">
+              <p className="client">{item.customer.name}</p>
+            </div>
+            <div className="utilitaire-phone">
+              <a href={`http://localhost:8080/driver/delivery/${item.id}`}>
+                <Button className="button" type="submit">Détails</Button>
+              </a>
+            </div>
+          </div>
+        </Mediaquery>
+      ))}
+    </div>
+  );
+};
 
 export default DriverList;
