78ea35a26ba9b471e64aa4e96b554e3548e11047
SylvainLEFORT <Lefort10700@hotmail.com>
2022-04-15 11:15:08 +0200

    adjusment in driver component

--- src/components/Driver/index.js
+++ src/components/Driver/index.js
@@ -29,51 +29,54 @@ const Driver = () => {
     <div>
       <div>
         <div className="global">
+          <h1 className="titre">Mes livraisons</h1>
           {deliveries && deliveries.map((item) => (
-            <Mediaquery minWidth={601}>
-              <h1 className="titre">Mes livraisons</h1>
-              <a href={`http://localhost:8080/driver/delivery/${item.id}`}>
-                <Button className="button detail" type="submit">
-                  <div className="infosClient">
-                    <p className="client">{item.customer.name}</p>
-                    <p className="adresse">{item.customer.address}</p>
-                  </div>
-                  <div className="utilitaire">
-                    <p className="statut">{(() => {
-                      switch (item.status) {
-                        case 0: return 'En attende de livraison';
-                        case 1: return 'En cours de livraison';
-                        default: return '';
-                      }
-                    })()}
-                    </p>
-                  </div>
-                </Button>
-              </a>
-            </Mediaquery>
+            <div className="driver-deliveries">
+              <Mediaquery minWidth={601}>
+                <a href={`http://localhost:8080/driver/delivery/${item.id}`}>
+                  <Button className="button detail" type="submit">
+                    <div className="infosClient">
+                      <p className="client">{item.customer.name}</p>
+                      <p className="adresse">{item.customer.address}</p>
+                    </div>
+                    <div className="utilitaire">
+                      <p className="statut">{(() => {
+                        switch (item.status) {
+                          case 0: return 'En attende de livraison';
+                          case 1: return 'En cours de livraison';
+                          default: return '';
+                        }
+                      })()}
+                      </p>
+                    </div>
+                  </Button>
+                </a>
+              </Mediaquery>
+            </div>
           ))}
         </div>
 
         <div>
           {deliveries && deliveries.map((item) => (
-            <Mediaquery maxWidth={600}>
-              <h1 className="title-phone">Mes livraisons</h1>
-              <a href={`http://localhost:8080/driver/delivery/${item.id}`}>
-                <Button className="button detail" type="submit">
-                  <div className="infosClient-phone">
-                    <p className="client">{item.customer.name}</p>
-                    <p className="client">{(() => {
-                      switch (item.status) {
-                        case 0: return 'En attende de livraison';
-                        case 1: return 'En cours de livraison';
-                        default: return '';
-                      }
-                    })()}
-                    </p>
-                  </div>
-                </Button>
-              </a>
-            </Mediaquery>
+            <div>
+              <Mediaquery maxWidth={600}>
+                <a href={`http://localhost:8080/driver/delivery/${item.id}`}>
+                  <Button className="button detail" type="submit">
+                    <div className="infosClient-phone">
+                      <p className="client">{item.customer.name}</p>
+                      <p className="client">{(() => {
+                        switch (item.status) {
+                          case 0: return 'En attende de livraison';
+                          case 1: return 'En cours de livraison';
+                          default: return '';
+                        }
+                      })()}
+                      </p>
+                    </div>
+                  </Button>
+                </a>
+              </Mediaquery>
+            </div>
           ))}
         </div>
       </div>
