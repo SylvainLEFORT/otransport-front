c58b2773f369a64e4c07425e29a28a18d2b67310
SylvainLEFORT <Lefort10700@hotmail.com>
2022-04-20 15:58:58 +0200

    build

--- src/components/Developpers/index.js
+++ src/components/Developpers/index.js
@@ -16,19 +16,19 @@ const Developpers = () => (
         <div className="div-img"><img src={sylvain} alt="Sylvain" /></div>
         <p className="team-name">Lefort Sylvain</p>
         <p className="team-title">Lead Front</p>
-        <p className="team-description">Franchement ... je saurais pas dire</p>
+        <p className="team-description">C’est votre attitude, bien plus que votre aptitude, qui détermine votre altitude.</p>
       </div>
       <div className="div">
         <div className="div-img"><img src={celia} alt="Célia" /></div>
         <p className="team-name">Tosic Célia</p>
         <p className="team-title">Lead Back</p>
-        <p className="team-description">Le dev, c'est comme le chocolat, plus on en mange, plus on aime ça</p>
+        <p className="team-description">Le dev, c'est comme le chocolat, plus on en mange, plus on aime ça.</p>
       </div>
       <div className="div">
         <div className="div-img"><img src={francois} alt="Francois" /></div>
         <p className="team-name">Boudin François</p>
         <p className="team-title">Product Owner</p>
-        <p className="team-description">Si le silence est d'or alors je le laisse a ceux qui ont en besoin !</p>
+        <p className="team-description">Si le silence est d'or alors je le laisse a ceux qui en ont besoin !</p>
       </div>
       <div className="div">
         <div className="div-img"><img src={joffrey} alt="Joffrey" /></div>
